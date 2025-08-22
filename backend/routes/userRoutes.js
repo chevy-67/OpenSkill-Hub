adafe3e4f4d49fed368f386cbf7a70e04732f67e
bharathsekar7777@gmail.com <bharathsekar7777@gmail.com>
2025-08-22 23:54:35 +0530

    minor bug fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 router.post("/login", async (req, res) => {
   try {
-    const { username, pass } = req.body;
+    const { username, password } = req.body;
     let user = await User.findOne({ username });
 
     if (!user) {
       return res.status(404).json({ error: "User not exist" });
     }
-    if (pass != user.password) {
-      return res.status(400).json({ error: "Wrong password" });
+    if (password != user.password) {
+      return res.status(400).json({ error: "Wrong password"});
     }
     res.status(200).json("Login succesfull", user);
   } catch (err) {
@@ -22,7 +22,7 @@ router.post("/login", async (req, res) => {
 
 router.post("/signup", async (req, res) => {
   try {
-    const { name, usernmae, email, pass } = req.body;
+    const { name, username, email, password } = req.body;
     let existUser = await User.findOne({ email });
     let existUsername = await User.findOne({ username });
     if (existUser) {
@@ -31,8 +31,8 @@ router.post("/signup", async (req, res) => {
     if (existUsername) {
       return res.status(400).json({ message: "Username not available" });
     }
-    const newUser = new User({ name, usernmae, email, pass });
-    User.save();
+    const newUser = new User({ name, username, email, password});
+    await newUser.save()
     res.status(201).json({ message: "Account created succesfullt" });
   } catch (er) {
     return res.status(500).json({ error: er.message });
