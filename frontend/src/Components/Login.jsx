2edcc624fd3f433902f377a68387fe3bfc4b21d9
bharathsekar7777@gmail.com <bharathsekar7777@gmail.com>
2025-08-25 20:04:18 +0530

    Add Authentication

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
-import { faGoogle, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { Link } from "react-router-dom";
 import './Login.css';
 
 function Login() {
+  const [creds,setCreds] = useState({
+    username:'',
+    password:''
+  })
+
+  const handleChange = (e)=>{
+    setCreds({...creds,[e.target.name]:e.target.value})
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    try{
+      const resp = await fetch('http://localhost:8989/api/users/login',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(creds)
+      })
+
+      const result = await resp.json()
+
+      if(resp.ok){
+        console.log(result.message)
+      }
+      else{
+        console.log(result.error)
+      }
+    }
+    catch(err){
+      console.log("Error : "+err)
+    }
+  }
+
   return (
     <>
-      <form className="login-container">
+      <form className="login-container" onSubmit={handleSubmit}>
         <header>Sign In</header>
         <p className="signup-link">
           New User? <Link to="/signup">Create an account</Link>
@@ -17,11 +48,11 @@ function Login() {
         <div className="input-group">
           <label htmlFor="username">
             <FontAwesomeIcon icon={faUser} className="icon"/>
-            <input type="text" id="username" placeholder="Username" />
+            <input type="text" name="username" value={creds.username} placeholder="Username" onChange={handleChange}/>
           </label>
           <label htmlFor="password">
             <FontAwesomeIcon icon={faLock} className="icon"/>
-            <input type="password" id="password" placeholder="Password" />
+            <input type="password" name="password" value={creds.password} placeholder="Password" onChange={handleChange}/>
           </label>
 
           <button type="submit">Sign In</button>
@@ -30,21 +61,6 @@ function Login() {
         <p className="forgot-password">
           <Link to="/forgot-password">Forgot your password?</Link>
         </p>
-
-        <div className="other-options">
-          <p>Or sign in with:</p>
-          <div className="social-icons">
-            <a href="#" className="google">
-              <FontAwesomeIcon icon={faGoogle} />
-            </a>
-            <a href="#" className="facebook">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a href="#" className="twitter">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-          </div>
-        </div>
       </form>
     </>
   );
