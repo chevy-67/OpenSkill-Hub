dd40daaf2f6d39d0ab5ec7b8cdf46e42e0f30528
bharathsekar7777@gmail.com <bharathsekar7777@gmail.com>
2025-08-25 18:42:05 +0530

    Add Backend Connectivity

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Signup.css'
 
 const Signup = () => {
+  const [formData,setFormData] = useState({
+    name : '',
+    username : '',
+    email : '',
+    password : ''
+  })
+
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const saveChange=async (e)=>{
+    e.preventDefault()
+    try{
+      const resp = await fetch('http://localhost:8989/api/users/signup',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(formData)
+      })
+      
+      const res = await resp.json()
+
+      if(resp.ok){
+        console.log(res.message)
+      }
+      else{
+        console.log("Error : "+res.error)
+      }
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div className='container'>
         <h2>Signup</h2>
-        <form className='form'>
+        <form className='form' onSubmit={saveChange}>
             <label>Name : </label>
-            <input type='text' id='name'/>
+            <input type='text' name='name' value={formData.name} onChange={handleChange}/>
             <label>Username : </label>
-            <input type='text' id='username'/>
+            <input type='text' name='username' value={formData.username} onChange={handleChange}/>
             <label>Email : </label>
-            <input type='email' id='email'/>
+            <input type='email' name='email' value={formData.email} onChange={handleChange}/>
             <label>Password : </label>
-            <input type='password' id='pass'/>
+            <input type='password' name='password' value={formData.password} onChange={handleChange}/>
             <label>Confirm Password : </label>
-            <input type='password' id='c_pass'/>
-            <button>Signup</button>
+            <input type='password' name='c_pass'/>
+            <button type='submit'>Signup</button>
         </form>
     </div>
   )
