376d84a9254cf494662c1a61c1c92b7333688993
bharathsekar7777@gmail.com <bharathsekar7777@gmail.com>
2025-09-01 00:20:53 +0530

    Modified

--- frontend/src/Components/CreatePost.jsx
+++ frontend/src/Components/CreatePost.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import {useState} from 'react'
+import '../styles/CreatePost.css'
 
 const CreatePost = () => {
+  const [postData,setPostData] = useState({
+    title : '',
+    description : ''
+  })
+
+  const handleChange = (e)=>{
+    setPostData({...postData,[e.target.name]:e.target.value})
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    try{
+      const resp = await fetch('http://localhost:8989/api/users/create_post',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(postData)
+      })
+      
+      const res = await resp.json()
+
+      if(resp.ok){
+        console.log(res.message)
+      }
+      else{
+        console.log("Error : "+res.error)
+      }
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div>
-        <form>
-            <lable>Title : </lable>
-            <input type='text' /><br/><br/>
-            <lable>Description : </lable>
-            <input type='textarea' /><br/><br/>
-            <button>Post</button>
-        </form>
+        <div className='form'>
+          <form onSubmit={handleSubmit}>
+            <label>Title : </label>
+            <input type='text' name='title' onChange={handleChange} value={postData.title} required />
+            <label>Description : </label>
+            <textarea name='description' onChange={handleChange} value={postData.description} required/>
+            <button type='submit'>Post</button>
+          </form>
+        </div>
     </div>
   )
 }
--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
-import './Login.css';
+import '../styles/Login.css';
 
 function Login() {
   const [creds,setCreds] = useState({
