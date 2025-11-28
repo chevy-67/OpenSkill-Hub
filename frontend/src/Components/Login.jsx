df18fc16bdfd5befcdcb8be8b01f712434e35da8
akilandeshwarann <akilandeshwarann.23aim@kongu.edu>
2025-11-28 09:18:25 +0530

    Files are changed

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,10 +1,13 @@
 import React,{useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
-import { Link } from "react-router-dom";
-import './Login.css';
+import { Link, useNavigate } from "react-router-dom";
+import '../styles/Login.css';
+
+const API_URL = import.meta.env.VITE_DEPLOY_URL
 
 function Login() {
+  const navigate = useNavigate()
   const [creds,setCreds] = useState({
     username:'',
     password:''
@@ -17,7 +20,7 @@ function Login() {
   const handleSubmit = async (e)=>{
     e.preventDefault()
     try{
-      const resp = await fetch('http://localhost:8989/api/users/login',{
+      const resp = await fetch(`${API_URL}/api/users/login`,{
         method:'POST',
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify(creds)
@@ -26,9 +29,13 @@ function Login() {
       const result = await resp.json()
 
       if(resp.ok){
-        console.log(result.message)
+        alert(result.message)
+        localStorage.setItem('username',creds.username)
+        localStorage.setItem('token',result.token)
+        navigate('/home')
       }
       else{
+        alert(result.error)
         console.log(result.error)
       }
     }
